4c75b86969ae1f474dfb323e5ae81d9fa9f5f246
baddger <start.964@gmail.com>
2018-07-16 21:10:46 +0200

    ajout des script pour capturer des video

---
+++ Pseudo_GPS/save_video180.py
@@ -0,0 +1,13 @@
+import picamera
+import datetime
+
+now = datetime.datetime.now()
+
+print(now.isoformat())
+
+exit
+with picamera.PiCamera() as camera:
+    camera.resolution = (640*2, 480*2)
+    camera.start_recording(now.isoformat() + '.h264')
+    camera.wait_recording(180)
+    camera.stop_recording()
